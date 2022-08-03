9847d9c95a599583778b6b621738df89ee5eb3e2
daninoks <daninoks@protonmail.com>
2022-08-03 11:33:10 +0400

    recent changes

--- task3/client.py
+++ task3/client.py
@@ -5,6 +5,7 @@ import lorem
 import random
 import json
 import re
+import string
 
 from token_generator import token_gen
 from info import ClientInfo
@@ -17,46 +18,58 @@ PORT_MSG = 8001         # The port used by the server for messages.
 
 c_info = ClientInfo()   # Client side client class.
 
+def main():
+    client = 0
+    while client < CLIENTS_NUM:
 
-client = 0
-while client < CLIENTS_NUM:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        identifier_token = token_gen(8)
+        # Init socket on client side:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            identifier_token = token_gen(8)
 
-        s.connect((HOST, PORT_KEYS))
-        s.sendall(identifier_token.encode('utf8'))
+            s.connect((HOST, PORT_KEYS))
+            s.sendall(identifier_token.encode('utf8'))
 
-        recived_token = s.recv(1024).decode('utf8')
+            recived_token = s.recv(1024).decode('utf8')
 
-        if recived_token:
-            c_info.add_token_pair(identifier_token, recived_token)
+            if recived_token:
+                c_info.add_token_pair(identifier_token, recived_token)
 
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
-                # Random message generator:
-                text = re.sub('\n', '', lorem.text())
-                textList = text.split(' ')
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
 
-                words_random = random.choices(textList, k = 4)
-                random_text = ''
-                for item in words_random:
-                    random_text += f'{item.strip()} '
+                    # Random message generator:
+                    text = re.sub('\n', '', lorem.text())
+                    textList = text.split(' ')
+                    words_random = random.choices(textList, k = 4)
+                    random_text = ''
+                    for item in words_random:
+                        random_text += f'{item.strip()} '
 
-                msgObj = c_info.save_msg(identifier_token, recived_token, random_text)
-                msgObj_encoded = json.dumps(msgObj).encode('utf-8')
+                    # Wrong recied token cases:
+                    if client < 10:
+                        wrong_recived_token = token_gen(size=8, chars=string.digits)
+                        msgObj = c_info.save_msg(identifier_token, wrong_recived_token, random_text)
+                    else:
+                        msgObj = c_info.save_msg(identifier_token, recived_token, random_text)
 
-                s1.connect((HOST, PORT_MSG))
-                s1.sendall(msgObj_encoded)
+                    msgObj_encoded = json.dumps(msgObj).encode('utf-8')
 
-                recived_respond = s1.recv(1024).decode('utf8')
-        else:
-            print('ERROR: respond Token not recived')
+                    s1.connect((HOST, PORT_MSG))
+                    s1.sendall(msgObj_encoded)
 
-        # print(c_info.users)
-        print(f'Client token --- {identifier_token}')
-        print(f'Server token --- {recived_token!r}')
-        print(f'Message: {random_text}')
-        print(f'{recived_respond}')
+                    recived_respond = s1.recv(1024).decode('utf8')
+            else:
+                print('ERROR: respond Token not recived')
+
+            terminal_feedback = format(
+                f'Client token --- {identifier_token}\n'
+                f'Server token --- {recived_token!r}\n'
+                f'Message: {random_text}\n'
+                f'{recived_respond}\n'
+            )
+
+            print(terminal_feedback)
+            client += 1
 
 
-        print('')
-        client += 1
+if __name__ == "__main__":
+    main()
--- task3/server.py
+++ task3/server.py
@@ -11,83 +11,94 @@ from info import ClientInfo
 
 
 HOST = "127.0.0.1"       # Standard loopback interface address (localhost)
+PORT_KEYS = 8000        # The port used by the server for tokens exchange.
+PORT_MSG = 8001         # The port used by the server for messages.
 
 c_info = ClientInfo()    # Server side client class.
+CLIENTS_NUM = 50
+
+def main():
+    def serve_on_port(port):
+
+        # Init socket on server side:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((HOST, port))
+            s.listen(CLIENTS_NUM)
+
+            if port == PORT_KEYS:
+                try:
+                    while True:
+                        clientsocket, address = s.accept()
+                        with clientsocket:
+                            print(f"Connected by {address}")
+                            recived_token = clientsocket.recv(1024).decode('utf8')
+
+                            # Checking token format:
+                            if len(recived_token) == 8:
+                                respond_token = token_gen(size=8, chars=string.digits)
+
+                                c_info.add_token_pair(recived_token, respond_token)
+
+                                clientsocket.sendall(respond_token.encode('utf8'))
+                            else:
+                                send_data = 'ERROR: Wrong identifier format! Must be 8 charters.'
+                                clientsocket.sendall(send_data.encode('utf8'))
+
+                except KeyboardInterrupt as err:
+                    print(err)
+                    print('Stoped: get KeyboardInterrupt signal')
+                finally:
+                    clientsocket.close()
+                    print('Clientsocket on server side closed.')
+
+            if port == PORT_MSG:
+                try:
+                    while True:
+                        clientsocket, address = s.accept()
+                        with clientsocket:
+                            print(f"Connected by {address}")
+
+                            # Get message from recied object:
+                            recivedObj = json.loads(clientsocket.recv(1024).decode('utf8'))
+                            identifier = list(recivedObj.keys())[0]
+                            recived_token = recivedObj[identifier]['respond']
+                            recived_message = recivedObj[identifier]['message']
+                            formated_message = f'{identifier}: {recived_message}\n'
+
+                            if c_info.check_token_pair(identifier, recived_token):
+                                response = 'SUCCES: Message recived. Keys accepted!'
+                                clientsocket.sendall(response.encode('utf8'))
+
+                                try:
+                                    #  Create logs if not exist:
+                                    if not os.path.exists('logs'):
+                                        os.mkdir('logs')
+                                        print(f'---> logs just created!')
+
+                                    with open(f'logs/messages.log', "a") as file_object:
+                                        # Append at the end of file:
+                                        file_object.write(formated_message)
+                                # If there is any permission issue:
+                                except PermissionError:
+                                    e = 'ERROR: Permission denied.'
+                                    print(e)
+
+                            else:
+                                response = 'ERROR: Message recived. Keys denied!'
+                                clientsocket.sendall(response.encode('utf8'))
 
 
-def serve_on_port(port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((HOST, port))
-        s.listen(50)
-        if port == 8000:
-            try:
-                while True:
-                    clientsocket, address = s.accept()
-                    with clientsocket:
-                        print(f"Connected by {address}")
+                except KeyboardInterrupt as err:
+                    print(err)
+                    print('Stoped: get KeyboardInterrupt signal')
+                finally:
+                    clientsocket.close()
+                    print('Clientsocket on server side closed.')
 
-                        recived_token = clientsocket.recv(1024).decode('utf8')
-
-                        if len(recived_token) == 8:
-                            respond_token = token_gen(size=8, chars=string.digits)
-
-                            c_info.add_token_pair(recived_token, respond_token)
-
-                            clientsocket.sendall(respond_token.encode('utf8'))
-                        else:
-                            send_data = 'ERROR: Wrong identifier format! Must be 8 charters.'
-                            clientsocket.sendall(send_data.encode('utf8'))
-
-            except KeyboardInterrupt as err:
-                print(err)
-                print('Stoped: get KeyboardInterrupt signal')
-            finally:
-                clientsocket.close()
-                print('Clientsocket on server side closed.')
-
-        if port == 8001:
-            try:
-                while True:
-                    clientsocket, address = s.accept()
-                    with clientsocket:
-                        print(f"Connected by {address}")
-
-                        recivedObj = json.loads(clientsocket.recv(1024).decode('utf8'))
-                        identifier = list(recivedObj.keys())[0]
-                        recived_token = recivedObj[identifier]['respond']
-                        recived_message = recivedObj[identifier]['message']
-                        formated_message = f'{identifier}: {recived_message}\n'
-
-                        if c_info.check_token_pair(identifier, recived_token):
-                            response = 'SUCCES: msg recived. keys accepted'
-                            clientsocket.sendall(response.encode('utf8'))
-
-                            try:
-                                #  Create logs if not exist:
-                                if not os.path.exists('logs'):
-                                    os.mkdir('logs')
-                                    print(f'---> logs just created!')
-
-                                with open(f'logs/messages.log', "a") as file_object:
-                                    # Append at the end of file:
-                                    file_object.write(formated_message)
-                            # If there is any permission issue:
-                            except PermissionError:
-                                e = 'ERROR: Permission denied.'
-                                print(e)
-
-                        else:
-                            response = 'ERROR: msg recived. keys denied'
-                            clientsocket.sendall(response.encode('utf8'))
+    Thread(target=serve_on_port, args=[8000]).start()
+    Thread(target=serve_on_port, args=[8001]).start()
 
 
-            except KeyboardInterrupt as err:
-                print(err)
-                print('Stoped: get KeyboardInterrupt signal')
-            finally:
-                clientsocket.close()
-                print('Clientsocket on server side closed.')
-
-Thread(target=serve_on_port, args=[8000]).start()
-Thread(target=serve_on_port, args=[8001]).start()
+if __name__ == "__main__":
+    main()
